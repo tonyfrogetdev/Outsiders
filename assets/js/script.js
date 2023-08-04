5f45e3bd5bbd94efdce5e841c2e2261d0b1273b9
tonyfrogetdev <tonyfrogetdev@gmail.com>
2023-08-04 15:09:50 +0200

    Header fini

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+
+
+/**
+ * MOBILE NAVBAR
+ * navbar will show after clicking menu button
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", toggleNav);
+
+const navClose = () => {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", navClose);
+
+
+
+/**
+ * HEADER and BACK TOP BTN
+ * header and back top btn will be active after scrolled down to 100px of screen
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeEl = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeEl);
